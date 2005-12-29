windygu/AnkhSVN

commit 2e9c892ee6e6cf0eeabd2c3715d8eca0b6fa24c0

Work around the ArgumentException being thrown when the solution is closed in VS2005 RTM
* /src/Ankh/ConflictManager.cs
	(RemoveAllTaskItems) - Swallow the ArgumentException and skip the rest of the function, assuming we're shutting down


git-svn-id: https://ctf.open.collab.net/svn/repos/ankhsvn/trunk@2303 ecf7f2a6-7b29-9143-b1c6-df2d17ec8657

src/Ankh/ConflictManager.cs
@@ -73,7 +73,16 @@ namespace Ankh
         ///          /// </summary>
         public void RemoveAllTaskItems()
         {
-            Window win =  this.context.DTE.Windows.Item(Constants.vsWindowKindTaskList);
+            try
+            {
+                Window win = this.context.DTE.Windows.Item(Constants.vsWindowKindTaskList);
+            }
+            catch(ArgumentException)
+            {
+                // Swallow (this occurs on VS2005 RTM shutdown)
+                return;
+            }
+
             TaskList taskList = (TaskList) win.Object;
             foreach(TaskItem item in taskList.TaskItems)
             {

commit 10fc39a2ce59a9f146d459c463afa2e59c5c7c49

Fix the build error introduced in r2303
* /src/ConflictManager.cs
	(RemoveAllTaskItems) - Declare Window outside of the try{} catch{} scope


git-svn-id: https://ctf.open.collab.net/svn/repos/ankhsvn/trunk@2304 ecf7f2a6-7b29-9143-b1c6-df2d17ec8657

src/Ankh/ConflictManager.cs
@@ -73,9 +73,10 @@ namespace Ankh
         ///          /// </summary>
         public void RemoveAllTaskItems()
         {
+            Window win;
             try
             {
-                Window win = this.context.DTE.Windows.Item(Constants.vsWindowKindTaskList);
+                win = this.context.DTE.Windows.Item(Constants.vsWindowKindTaskList);
             }
             catch(ArgumentException)
             {